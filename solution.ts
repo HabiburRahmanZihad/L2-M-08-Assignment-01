170f3542fd92e58165b067567eab8cbd06b36db3
Habibur Rahman Zihad <e241024@ugrad.iiuc.ac.bd>
2025-11-17 10:38:49 +0600

    solve problem 1

--- solution.ts
+++ solution.ts
@@ -1,1 +1,11 @@
-//make assignment of problems
+type TypeOfValue = string | number | boolean;
+
+const formatValue = (value: TypeOfValue): string | number | boolean => {
+  if (typeof value === 'string') {
+    return value.toUpperCase();
+  } else if (typeof value === 'number') {
+    return value * 10;
+  } else {
+    return !value;
+  }
+};
