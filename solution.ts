e17837e199d5a337bd1208eaea457e133096ec56
Habibur Rahman Zihad <e241024@ugrad.iiuc.ac.bd>
2025-11-17 11:19:31 +0600

    solve problem 5

--- solution.ts
+++ solution.ts
@@ -46,3 +46,15 @@ const filterByRating = (items: Item[]): Item[] => {
   return items.filter((item) => item.rating >= 4);
 
 };
+
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+}
+
+const filterActiveUsers = (users: User[]): User[] => {
+  return users.filter((user) => user.isActive === true);
+};
